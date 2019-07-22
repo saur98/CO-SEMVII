6a07dde11c334d24bfbc17dd287508de92e5acd8
Saurabh Maheshwari <saurabh.maheshwari1011@gmail.com>
2019-07-22 12:46:35 +0530

    Add files via upload

---
+++ 160420107048_dm/src/dm_7.java
@@ -0,0 +1,113 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Hashtable;
+
+public class dm_7 {
+
+	int choice = 0;
+	FileReader fin;
+	FileWriter fout;
+	BufferedReader bin;
+	BufferedWriter bout;
+	String[] cols;
+	ArrayList<String[]> dataList;
+	Double initalData[][];
+	String line = null;
+	int missingCol[];
+	float nmin,nmax;
+	Hashtable<String, Float> finaldata;
+	Hashtable<String, Float> finaldatas;
+
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		new dm_7();
+
+	}
+	
+	public dm_7() throws IOException
+	{
+		fileRead();
+		int flag=0;
+		for(String val:dataList.get(1))
+		{
+			if(flag==1)
+				nmin=Integer.valueOf(val);
+			else if(flag==2)
+				nmax=Integer.valueOf(val);	
+			flag++;
+		}
+		//nmin=Integer.valueOf(dataList[1][1]);
+		//nmax=dataList[1][2];
+		evaluate();
+		finaldatas = new Hashtable<String, Float>();
+		while(finaldata.size()!=0)
+		{
+			float maxValues=nmax;
+			String key="";
+			for (String data : finaldata.keySet()) {
+				if (finaldata.get(data) > maxValues) {
+					System.out.println(maxValues);
+					maxValues = finaldata.get(data);
+					key=data;
+				}
+			}
+			finaldatas.put(key, maxValues);
+			finaldata.remove(key);
+		}
+		//Collections.sort(finaldata);
+		//System.out.println(finaldata);
+		for (String data : finaldatas.keySet())
+		{
+			System.out.println(data+" "+finaldatas.get(data));
+		}
+	}
+	
+	private void fileRead() throws IOException {
+		dataList = new ArrayList<>();
+		fin = new FileReader(new File("assets/range.data"));
+		bin = new BufferedReader(fin);
+
+		while ((line = bin.readLine()) != null) {
+			cols = line.split(",");
+			//System.out.println(cols);
+			dataList.add(cols);
+
+		}
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void evaluate() throws IOException {
+		int flag=0;
+		finaldata = new Hashtable<String, Float>();
+		for(String[] rows:dataList)
+		{
+			if(flag<2)
+				flag++;
+			else
+			{
+				fin = new FileReader(new File("assets/"+rows[0]+".data"));
+				bin = new BufferedReader(fin);
+				while ((line = bin.readLine()) != null) {
+					cols=line.split(",");
+					float newval = ((Float.valueOf(cols[1])-Float.valueOf(rows[1]))/(Float.valueOf(rows[2])-Float.valueOf(rows[1])))*(nmax-nmin)+nmin;
+					//String[] values = new String[2];
+					//System.out.println(cols[1]+" "+rows[1]+" "+rows[2]+" "+nmin+" "+nmax+newval);
+					//values[0]=cols[0];
+					//values[1]=String.valueOf(newval);
+					//System.out.println(values[0]+" "+values[1]);
+					finaldata.put(cols[0],newval);
+					
+				}
+			}
+		}
+	}
+
+}
